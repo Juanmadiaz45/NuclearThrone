44bb8e7457b9242d1b139aa44f69a710acee31e6
Felipe Velasco <100039794+felipevelasco7@users.noreply.github.com>
2023-06-08 01:02:46 -0500

    fix some issues

--- src/main/java/com/example/nuclearthrone/model/Vector.java
+++ src/main/java/com/example/nuclearthrone/model/Vector.java
@@ -18,4 +18,19 @@ public class Vector {
         return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
